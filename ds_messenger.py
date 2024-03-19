cea16fe673aa04ac13a6d00e122866d95e823dc8
ekirchst <ekirchst@uci.edu>
2024-03-18 22:48:11 -0700

    uh oh spagheti

--- ds_messenger.py
+++ ds_messenger.py
@@ -1,6 +1,13 @@
 # Evan Kirchstter
 # ekirchst@uci.edu
 # 59946460
+import socket
+import time
+import json
+port = 3021
+timestamp = str(time.time())
+
+
 class DirectMessage:
   def __init__(self):
     self.recipient = None
@@ -11,9 +18,27 @@ class DirectMessage:
 class DirectMessenger:
   def __init__(self, dsuserver=None, username=None, password=None):
     self.token = None
+    self.dsuserver = dsuserver
+    self.username = username
+    self.password = password
 		
   def send(self, message:str, recipient:str) -> bool:
     # must return true if message successfully sent, false if send failed.
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_conn:
+        server_conn.connect((self.dsuserver, port))
+        formated = ({
+                "token": self.token,
+                "directmessage": {
+                    "entry": message,
+                    "recipient": recipient,
+                    'timestamp': timestamp
+                }
+                })
+        data_str = json.dumps(formated)
+        server_conn.sendall(data_str.encode())
+        response = server_conn.recv(3021).decode()
+        response_json = json.loads(response)
+        print(response_json)
     pass
 		
   def retrieve_new(self) -> list:
--- test_ds_messenger.py
+++ test_ds_messenger.py
@@ -1,62 +1,87 @@
 import socket
 import json
 import time
+from ds_messenger import DirectMessage, DirectMessenger
+
 server = "168.235.86.101"
 port = 3021
 timestamp = str(time.time())
 
+def start():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_conn:
+        server_conn.connect((server, port))
+        stuff = {}
+        stuff["join"] = {
+                        "username": 'greenmmm',
+                        "password": 'heheha',
+                        "token": ""
+                    }
+        data_str = json.dumps(stuff)
+        server_conn.sendall(data_str.encode())
+        response = server_conn.recv(3021).decode()
+        response_json = json.loads(response)
+        print(response_json)   
+        if "token" in str(response_json):
+                    temp = str(response_json).index("token")
+                    token = str(response_json)[temp+9:-3]
+    
+        formated = DirectMessage()
+        formated.recipient = 'help'
+        formated.message = 'fuck you bitch'
+        formated.timestamp = timestamp
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_conn:
-            server_conn.connect((server, port))
-            stuff = {}
-            stuff["join"] = {
-                    "username": 'greenmmm',
-                    "password": 'heheha',
-                    "token": ""
+        mew = DirectMessenger("168.235.86.101", 'greenmmm', 'heheha')
+        mew.token = token
+
+        mew.send("fuck you bitch", 'help')
+
+
+
+
+
+
+
+
+
+    '''
+        
+        formated = ({
+                "token": token,
+                "directmessage": {
+                    "entry": 'greenfn',
+                    "recipient": 'help',
+                    'timestamp': timestamp
                 }
-            print("joined")
-            data_str = json.dumps(stuff)
-            server_conn.sendall(data_str.encode())
-            response = server_conn.recv(3021).decode()
-            response_json = json.loads(response)
-            print(response_json)
-            if "token" in str(response_json):
-                temp = str(response_json).index("token")
-                token = str(response_json)[temp+9:-3]
-            formated = ({
-            "token": token,
-            "directmessage": {
-                "entry": 'greenfn',
-                "recipient": 'help',
-                'timestamp': timestamp
-            }
-            })
-            data_str = json.dumps(formated)
-            server_conn.sendall(data_str.encode())
-            response = server_conn.recv(3021).decode()
-            response_json = json.loads(response)
-            print(response_json)
-            
-            
-            read = ({
-            "token": token,
-            "directmessage": 'new'
-            })
+                })
 
-            data_str = json.dumps(read)
-            server_conn.sendall(data_str.encode())
-            response = server_conn.recv(3021).decode()
-            response_json = json.loads(response)
-            print(response_json)
+        data_str = json.dumps(formated)
+        server_conn.sendall(data_str.encode())
+        response = server_conn.recv(3021).decode()
+        response_json = json.loads(response)
+        print(response_json)
+                
+        read = ({
+                "token": token,
+                "directmessage": 'new'
+                })
 
+        data_str = json.dumps(read)
+        server_conn.sendall(data_str.encode())
+        response = server_conn.recv(3021).decode()
+        response_json = json.loads(response)
+        print(response_json)
 
-            read = ({
-            "token": token,
-            "directmessage": 'all'
-            })
-            
-            data_str = json.dumps(read)
-            server_conn.sendall(data_str.encode())
-            response = server_conn.recv(3021).decode()
-            response_json = json.loads(response)
-            print(response_json)
+        read = ({
+                "token": token,
+                "directmessage": 'all'
+                })
+                
+        data_str = json.dumps(read)
+        server_conn.sendall(data_str.encode())
+        response = server_conn.recv(3021).decode()
+        response_json = json.loads(response)
+        print(response_json)
+'''
+
+if __name__ == '__main__':
+        start()
